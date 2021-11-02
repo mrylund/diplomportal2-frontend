854908af2b8f3a51bbb4362e45c9fec5f8fcaed8
Patrick <patrick.lopdrup@gmail.com>
2021-11-02 11:16:05 +0100

    Hardcoded the dtu auth string to redirect to our backend

--- src/Components/Navigationbar/Login.js
+++ src/Components/Navigationbar/Login.js
@@ -8,14 +8,11 @@ export const Login = () => {
     const handleClick = async () => {
         console.log("logger ind")
         const response = await logIn()
-        console.log("har ventet log ind")
-        window.location.replace("http://localhost:3000/")
-        console.log("login:", response.data.token)
     }
 
     return (
         //<Nav.Link onClick={handleClick}>Log In</Nav.Link>
-        <Nav.Link href="https://auth.dtu.dk/dtu/?service=http://localhost:443/login">
+        <Nav.Link href="https://auth.dtu.dk/dtu/?service=http://localhost:443/verifyticket">
             Log In
         </Nav.Link>
     )
