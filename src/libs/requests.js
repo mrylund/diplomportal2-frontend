d86b98d01a1fff268251c96a3358c697a65b5cdc
Rasmus <s152716@student.dtu.dk>
2021-11-11 23:38:36 +0100

    implement new endpoints in frontend + default objects for student and course

--- src/libs/requests.js
+++ src/libs/requests.js
@@ -26,14 +26,33 @@ export const getStudents = () =>
 export const getStudent = (id) =>
     query('get', basepath + `students/${id}`)
 
+export const createStudent = (student) =>
+    query('post', basepath + 'students', student)
+
 export const getCourses = () =>
     query('get', basepath + 'courses')
 
 export const getCourse = (id) =>
     query('get', basepath + `courses/${id}`)
 
+export const createCourse = (course) =>
+    query('post', basepath + 'courses', course)
+
 export const logIn = () =>
     query('get', basepath + 'login')
 
 export const logOut = () =>
     query('get', 'https://auth.dtu.dk/dtu/logout')
+
+
+export const defaultStudent = {
+    name: '',
+    studyNumber: '' // TODO: check if studyNumber is unique
+}
+
+export const defaultCourse = {
+    courseNumber: '',
+    title: '',
+    weekDay: '', // Format: first two letters of the day, e.g. 'ma' + 0, 1 or 2 describing the schedule period. E.g. 'ma0' for Monday in period 0.
+    sheets: '' // Google Sheets id, e.g. 1SxaB7WiuMFWqgWu3OuKJPc2hZ9_7tzLH58we0bhvDoo
+}
